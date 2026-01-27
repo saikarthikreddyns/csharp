644160850dbc703692a6ca872108f214a012b8de
saikarthikreddyns <141983186+saikarthikreddyns@users.noreply.github.com>
2026-01-28 03:11:20 +0530

    Added C# practice programs

--- practices/floydLoop.cs
+++ practices/floydLoop.cs
@@ -1,66 +1,47 @@
-resource "azurerm_virtual_network" "hub_vnet" {
-  name                = "hub-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.security_rg.name
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
 
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
+using System;
+
+public class HelloWorld
+{
+public static int FloydCycle(int[] arr)
+{
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do
+    {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+    
+    slow = arr[0];
+    while(slow!=fast){
+        slow=arr[slow];
+        fast=arr[fast];
+    }
+    return slow;
 }
 
-resource "azurerm_subnet" "mgmt_subnet" {
-  name                 = "mgmt-subnet"
-  resource_group_name  = azurerm_resource_group.security_rg.name
-  virtual_network_name = azurerm_virtual_network.hub_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+    public static void Main(string[] args)
+    {
+       int[][] testCases =
+{
+    new int[]{1,3,4,2,2},
+    new int[]{3,1,3,4,2},
+    new int[]{1,1,2,3,4},
+    new int[]{1,2,3,4,4},
+    new int[]{1,1},
+    new int[]{5,4,3,2,1,3},
+    new int[]{2,5,9,6,9,3,8,9,7,1}
+};
+
+foreach (var test in testCases)
+{
+    Console.WriteLine(FloydCycle(test));
 }
 
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.security_rg.name
-  virtual_network_name = azurerm_virtual_network.hub_vnet.name
-  address_prefixes     = ["10.0.2.0/26"]
+    }
 }
 
-resource "azurerm_network_security_group" "mgmt_nsg" {
-  name                = "mgmt-nsg"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.security_rg.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "mgmt_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.mgmt_subnet.id
-  network_security_group_id = azurerm_network_security_group.mgmt_nsg.id
-}
-
-resource "azurerm_public_ip" "bastion_pip" {
-  name                = "bastion-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.security_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
-}
